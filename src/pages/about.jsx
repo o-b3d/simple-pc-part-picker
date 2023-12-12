61e8b12a0611caaada7c798d8fad1dd0dcaed754
o-b3d <ojg0827@gmail.com>
2023-12-12 12:51:20 -0500

    began styling about us page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,30 @@
 export default function About() {
     return (
-        <div>
-            <h2>About Page</h2>
-            <p>This is the About page.</p>
+        <div class="py-16 bg-white">
+            <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div class="md:5/12 lg:w-5/12">
+                        <img
+                            src="https://images.unsplash.com/photo-1612833603922-5e9b5f8b9b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGV2ZWxvcG1lbnR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+                            alt="image"
+                            loading="lazy"
+                            width=""
+                            height=""
+                        />
+                    </div>
+                    <div class="md:7/12 lg:w-6/12">
+                        <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">
+                            About Us Page
+                        </h2>
+                        <p class="mt-6 text-gray-600">
+                            First Paragraph
+                        </p>
+                        <p class="mt-4 text-gray-600">
+                            Second Paragraph
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
