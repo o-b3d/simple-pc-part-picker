de5534a8aed0f594a98431d85cd1300e11452596
o-b3d <ojg0827@gmail.com>
2023-12-15 12:22:50 -0500

    changed config again

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
-            external: ['react-redux'],
+            external: ['react-redux', '@reduxjs/toolkit'],
         },
     },
 })
